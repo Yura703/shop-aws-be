c13fd7510d781b82d9886f67f7e8957604fb9c0a
Yura Lapitski <lyurik@tut.by>
2022-09-25 15:53:53 +0300

    feat: add postgres db settings

--- product-service/functions/getProductsByID/getProductsByID.js
+++ product-service/functions/getProductsByID/getProductsByID.js
@@ -1,4 +1,18 @@
-import products from '../productList.json';
+//import products from '../productList.json';
+import { Client } from 'pg';
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+};
 
 const handlerGetByID = (products = {}, status = 200) => ( 
   {
@@ -12,13 +26,29 @@ const handlerGetByID = (products = {}, status = 200) => (
   }
 );
 
-export const handler = async (event) => {
-  const { productId } = event.pathParameters || null;
+// export const handler = async (event) => {
+//   const { productId } = event.pathParameters || null;
 
-  const product = await products.find(({ id }) => id === productId);
+//   const product = await products.find(({ id }) => id === productId);
   
-  if (!product)  { 
-    return handlerGetByID({ message: 'Error: Product not found!' }, 400);
+//   if (!product)  { 
+//     return handlerGetByID({ message: 'Error: Product not found!' }, 400);
+//   }
+//     return handlerGetByID({ ...product }, 200);
+// };
+
+export const handler = async event => {
+  const { productId } = event.pathParameters || null;
+  const client = new Client(dbOptions);
+  await client.connect();
+
+  try {
+    const { rows: product } = await client.query(`select * from products where id='${productId}'`);
+    return await handlerGetByID(product, 200);
+
+  } catch (error) {
+    console.error('Error: ' + error);
+  } finally {
+    client.end();
   }
-    return handlerGetByID({ ...product }, 200);
-};
+}
